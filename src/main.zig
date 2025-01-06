ba65927b95ee52e753129685753b2b0c1a8478b9
Roger Esteve <rogeresteve30@gmail.com>
2025-01-06 17:44:43 +0100

    window

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,32 @@
-const std = @import("std");
+const rl = @import("raylib");
 
-const Question = struct {
-    question: []const u8,
-    answer: []const u8,
-};
+pub fn main() anyerror!void {
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const screenWidth = 800;
+    const screenHeight = 450;
 
-const Questions = [_]Question{
-    Question{ .question = "What is your name?", .answer = "John" },
-    Question{ .question = "What is your age?", .answer = "25" },
-    Question{ .question = "What is your favorite color?", .answer = "Blue" },
-};
+    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    defer rl.closeWindow(); // Close window and OpenGL context
 
-pub fn main() !void {
-    try askQuestion();
-}
+    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
 
-pub fn askQuestion() !void {
-    // create allocator
-    const allocator = std.heap.page_allocator;
+    // Main game loop
+    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
 
+        // Draw
+        //----------------------------------------------------------------------------------
+        rl.beginDrawing();
+        defer rl.endDrawing();
 
-    for (Questions) |question| {
-        std.debug.print("{s}\n", .{question.question});
-        const answer = try readLine(&allocator);
-        if (std.mem.eql(u8, answer, question.answer)) {
-            std.debug.print("Correct!\n", .{});
-        } else {
-            std.debug.print("Incorrect! The correct answer is {s}.\n", .{question.answer});
-        }
-        allocator.free(answer);
+        rl.clearBackground(rl.Color.white);
+
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+        //----------------------------------------------------------------------------------
     }
-}
-
-pub fn readLine(allocator: *const std.mem.Allocator) ![]const u8 {
-    var stdin_file = std.io.getStdIn();
-    var stdin = stdin_file.reader();
-
-    // Read until newline (up to 1024 bytes):
-    var buff: []u8 = try allocator.alloc(u8, 1024);
-    if(try stdin.readUntilDelimiterOrEof(buff[0..], '\n')) |err| {
-        return err;
-    }
-
-    return buff;
 }
